f79b1478e056988e5b63dfc8235630a702385fbc
svdu <sdurnev@gmail.com>
2020-02-21 23:20:53 +0600

    Init

--- main.go
+++ main.go
@@ -25,67 +25,67 @@ type param struct {
 type opus_param []param
 
 var data opus_param = opus_param{
-	{1, 2, "Data_version_counter", []string{"Data_version_counter"}},
-	{2, 1, "Operating_mode", []string{
-		"2_0_Float_char_act",
-		"2_1_Bat_test_act",
-		"2_2_Boost_char_act",
-		"2_3_Temp_compen_act"}},
-	{3, 1, "Battery_test_state", []string{
-		"3_0_Perio_bat_test ",
-		"3_1_Manual bat_test",
-		"3_2_Natur_bat_test ",
-		"3_3_Rem_bat_test   ",
+	{1, 2, "DataVersionCounter", []string{"DataVersionCounter"}},
+	{2, 1, "OperatingMode", []string{
+		"2_0_FloatCharAct",
+		"2_1_BatTestAct",
+		"2_2_BoostCharAct",
+		"2_3_TempCompenAct"}},
+	{3, 1, "BatteryTestState", []string{
+		"3_0_PerioBatTest",
+		"3_1_ManualBatTest",
+		"3_2_NaturBatTest",
+		"3_3_RemBatTest",
 	}},
-	{4, 1, "Boost_charge_state", []string{
-		"4_0_Auto_boost_char ",
-		"4_1_Perio_boost_char",
-		"4_2_Man_boost_char  ",
-		"4_3_Rem_boost_char  ",
+	{4, 1, "BoostChargeState", []string{
+		"4_0_AutoBoostChar ",
+		"4_1_PerioBoostChar",
+		"4_2_ManBoostChar  ",
+		"4_3_RemBoostChar  ",
 	}},
-	{9, 2, "System_Voltage", []string{"bcmSystemVoltage"}},
-	{10, 2, "Load_current", []string{"bcmLoadCurrent"}},
-	{11, 2, "Battery_current", []string{"bcmBatteryCurrent"}},
-	{12, 2, "Total_rectifier_current", []string{"bcmTotalRectifierCurrent"}},
-	{13, 2, "Total_inverter_current", []string{"bcmTotalInverterCurrent"}},
-	{14, 2, "Maximum_battery_temperature", []string{"bcmMaxBatteryTemperature"}},
-	{15, 2, "Maximum_system_temperature", []string{"bcmMaxSystemTemperature"}},
-	{29, 1, "System voltage alarms", []string{
-		"30_0_Mains_fault",
-		"30_1_Phase_fault",
-		"30_2_Low_sys_volt",
-		"30_3_High_sys_volt",
-		"30_4_Float_char_devi",
-		"30_5_Invert_sys_mains_fault",
+	{9, 2, "SystemVoltage", []string{"bcmSystemVoltage"}},
+	{10, 2, "LoadCurrent", []string{"bcmLoadCurrent"}},
+	{11, 2, "BatteryCurrent", []string{"bcmBatteryCurrent"}},
+	{12, 2, "TotalRectifierCurrent", []string{"bcmTotalRectifierCurrent"}},
+	{13, 2, "TotalInverterCurrent", []string{"bcmTotalInverterCurrent"}},
+	{14, 2, "MaximumBatteryTemperature", []string{"bcmMaxBatteryTemperature"}},
+	{15, 2, "MaximumSystemTemperature", []string{"bcmMaxSystemTemperature"}},
+	{29, 1, "SystemVoltageAlarms", []string{
+		"30_0_MainsFault",
+		"30_1_PhaseFault",
+		"30_2_LowSysVolt",
+		"30_3_HighSysVolt",
+		"30_4_FloatCharDevi",
+		"30_5_InvertSysMainsFault",
 	}},
-	{30, 1, "System fault alarms", []string{
-		"31_0_Earth_fault",
-		"31_1_Load_fuse_fault",
-		"31_2_Bat_fuse_fault",
-		"31_3_Rectif_overlo",
-		"31_4_Invert_overlo",
-		"31_5_Bus_fault",
-		"31_6_DCP_Bus_Fault",
-		"31_7_Shunt_fault",
-		"31_8_Sys_over_temp",
-		"31_9_No_sys_temp_sens",
+	{30, 1, "SystemFaultAlarms", []string{
+		"31_0_EarthFault",
+		"31_1_LoadFuseFault",
+		"31_2_BatFuseFault",
+		"31_3_RectifOverlo",
+		"31_4_InvertOverlo",
+		"31_5_BusFault",
+		"31_6_DCPBusFault",
+		"31_7_ShuntFault",
+		"31_8_SysOverTemp",
+		"31_9_NoSysTempSens",
 	}},
-	{31, 1, "Miscellaneous system alarms", []string{
-		"32_0_Boost_charge_act",
-		"32_1_Config_confl",
-		"32_2_Invent_full",
+	{31, 1, "MiscellaneousSystemAlarms", []string{
+		"32_0_BoostChargeAct",
+		"32_1_ConfigConfl",
+		"32_2_InventFull",
 	}},
-	{32, 1, "Rectifier alarms", []string{
-		"33_0_Comm_error",
-		"33_1_Nvram_fault",
-		"33_2_Config_fault",
-		"33_3_Module_fault",
-		"33_4_Bad_firmware",
-		"33_5_Rectif_fault",
-		"33_6_Rectif_over_vol",
-		"33_7_Rectif_over_temp",
-		"33_8_Rectif_mains_fault",
-		"33_9_Rectif_wrong_vol_vers",
+	{32, 1, "RectifierAlarms", []string{
+		"33_0_CommError",
+		"33_1_NvramFault",
+		"33_2_ConfigFault",
+		"33_3_ModuleFault",
+		"33_4_BadFirmware",
+		"33_5_RectifFault",
+		"33_6_RectifOverVol",
+		"33_7_RectifOverTemp",
+		"33_8_RectifMainsFault",
+		"33_9_RectifWrongVolVers",
 	}},
 	{33, 1, "Inverter system alarms", []string{
 		"34_0_Comm_error",
@@ -151,22 +151,49 @@ func main() {
 		fmt.Printf("{\"status\":\"error\", \"error\":\"%s\", \"version\": \"%s\"}", err, version)
 	}
 
+	fmt.Println(results)
+
 	var tempStringArr []string
 	for i := 0; i < len(data); i++ {
 		reg := data[i].Req * 2
-		regData := results[reg : reg+2]
-		if i > 3 && i < 11 {
-			var par = strconv.FormatFloat(float64((float32(binary.BigEndian.Uint16(regData)) / 10)), 'f', 2, 32)
+		regData := binary.BigEndian.Uint16(results[reg : reg+2])
+		if i == 0 {
+			var par = strconv.FormatFloat(float64((float32(regData))), 'f', 2, 32)
+			tmpstr := []string{"\"", data[i].Par[0], "\": ", par}
+			var str = strings.Join(tmpstr, "")
+			tempStringArr = append(tempStringArr, str)
+		} else if i > 3 && i < 11 {
+			var par = strconv.FormatFloat(float64((float32(regData) / 10)), 'f', 2, 32)
 			tmpstr := []string{"\"", data[i].Par[0], "\": ", par}
 			var str = strings.Join(tmpstr, "")
 			tempStringArr = append(tempStringArr, str)
 		} else {
-
+			//var tStrAr []string
+			fmt.Print(data[i].Req)
+			fmt.Print(" ")
+			fmt.Println(data[i].Name)
+			//fmt.Println(regData)
+			for l := 0; l < 16; l++ {
+				if regData&(1<<l) != 0 {
+					fmt.Println(data[i].Par[l])
+					//mesage = append(mesage, listOfAllarm1[i+1])
+				}
+				//fmt.Println(data[i].Par[l])
+			}
 		}
 	}
-	fmt.Println(len(tempStringArr))
+	//fmt.Println(len(tempStringArr))
 	fmt.Println(tempStringArr)
 }
+
+/*
+for i := uint(0); i < 16; i++ {
+if a&(1<<i) != 0 {
+//fmt.Println(i)
+mesage = append(mesage, listOfAllarm1[i+1])
+}
+}
+*/
 
 func MekeFlotPar(reg int, data []byte) string {
 	//var retunData string
